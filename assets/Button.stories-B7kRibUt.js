c376851a1f6194eb16a68b3e03f458e780eac187
DHCross <DHCross@users.noreply.github.com>
2025-11-20 01:05:37 +0000

    deploy: 912b8ae0bcc85a838fa016a7d0a14a7deb52e505

--- assets/Button.stories-B7kRibUt.js
+++ assets/Button.stories-B7kRibUt.js
@@ -1,4 +1,4 @@
-import{B as t}from"./Button-C7zgKJex.js";import"./iframe-4QuVycNL.js";import"./preload-helper-PPVm8Dsz.js";const{fn:n}=__STORYBOOK_MODULE_TEST__,i={title:"Example/Button",component:t,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:n()}},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},s={args:{size:"small",label:"Button"}},o={args:{primary:!1,label:"Button"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+import{B as t}from"./Button-CkL6Znjv.js";import"./iframe-7iKl-0sm.js";import"./preload-helper-PPVm8Dsz.js";const{fn:n}=__STORYBOOK_MODULE_TEST__,i={title:"Example/Button",component:t,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:n()}},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},s={args:{size:"small",label:"Button"}},o={args:{primary:!1,label:"Button"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
   args: {
     primary: true,
     label: 'Button'
